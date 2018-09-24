8f146640709a7957b412285defcbe1b573dfc568
charpie <charpie@ufl.edu>
2018-09-24 12:22:50 -0400

    last touches before mvp; fixed the about and contact page, populated them

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function resize()
 		$('h2').each(function() { $(this).css('font-size', '3.5vw')});
 		$('#rights').each(function() { $(this).css('font-size', '2.25vw')});
 		$('#about').each(function() { $(this).css('font-size', '1.25vw')});
+		$('#contact').each(function() { $(this).css('font-size', '1.25vw')});
 		$('#right-panel').each(function() { $(this).css('flex-direction', 'row')});
 		$('#profile').each(    function() { $(this).css('height', '30%')});
 		$('#profile').each(    function() { $(this).css('width', '30%')});
@@ -15,7 +16,8 @@ function resize()
 		$('li').each(function() { $(this).css('font-size', '6vh')});
 		$('h2').each(function() { $(this).css('font-size', '7vh')});
 		$('#rights').each(function() { $(this).css('font-size', '4.5vh')});
-		$('#about').each(function() { $(this).css('font-size', '2.5vh')});
+		$('#about').each(function() { $(this).css('font-size', '1.85vh')});
+		$('#contact').each(function() { $(this).css('font-size', '2.5vh')});
 		$('#right-panel').each(function() { $(this).css('flex-direction', 'column')});
 		$('#profile').each(function() { $(this).css('height', '80%')});
 		$('#profile').each(function() { $(this).css('width', '80%')});
